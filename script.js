292aceb8b22ba4a8ed66ea2fe3514ac222f592c9
RickDemagic <rickdemagic@web.de>
2026-03-03 15:47:38 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const elements = document.querySelectorAll('.fade');
-
-window.addEventListener('scroll',()=>{
-elements.forEach(el=>{
-if(el.getBoundingClientRect().top < window.innerHeight - 100){
-el.classList.add('visible');
-}
-});
-});
