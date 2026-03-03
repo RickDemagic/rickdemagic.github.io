d232ee3d5c08fd0a6542967d566a8ae23c75d1fc
RickDemagic <rickdemagic@web.de>
2026-03-03 17:35:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Business Layout geladen.");
+document.addEventListener("DOMContentLoaded", function(){
+
+// Hero Fade-In
+const heroContent = document.querySelector(".hero-content");
+setTimeout(() => {
+    heroContent.classList.add("active");
+}, 300);
+
+// Scroll Animation
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("visible");
+        }
+    });
+},{
+    threshold:0.2
+});
+
+document.querySelectorAll(".card").forEach(card => {
+    observer.observe(card);
+});
+
+});
